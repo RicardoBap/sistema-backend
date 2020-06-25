7ca31f097d30876e07f92f79148e5dfa21e52e39
Ricardo <ric_bap@hotmail.com>
2020-06-25 18:23:07 -0300

    Produto associação muito para muitos

--- src/main/java/com/ricbap/sistema/SistemaApplication.java
+++ src/main/java/com/ricbap/sistema/SistemaApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ricbap.sistema.domain.Categoria;
+import com.ricbap.sistema.domain.Produto;
 import com.ricbap.sistema.repositories.CategoriaRepository;
+import com.ricbap.sistema.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class SistemaApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
@@ -22,10 +27,23 @@ public class SistemaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
