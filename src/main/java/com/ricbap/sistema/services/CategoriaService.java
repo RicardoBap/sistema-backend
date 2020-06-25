a31ae2bc0150bd94020dd72d1fef00ecc421c202
Ricardo <ric_bap@hotmail.com>
2020-06-25 18:56:46 -0300

    Ajustes no endpoint /categorias/{id} - ExceptionHandler

--- src/main/java/com/ricbap/sistema/services/CategoriaService.java
+++ src/main/java/com/ricbap/sistema/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ricbap.sistema.domain.Categoria;
 import com.ricbap.sistema.repositories.CategoriaRepository;
+import com.ricbap.sistema.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,10 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id +
+					", Tipo: " + Categoria.class.getName());			
+		});
 	}
 
 }
