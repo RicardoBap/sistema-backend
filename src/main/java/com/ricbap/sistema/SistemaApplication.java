1d684ac0b89ff08f8ffa460405284dcddd8dd66f
Ricardo <ric_bap@hotmail.com>
2020-06-25 17:41:51 -0300

    Criando operação de instanciação

--- src/main/java/com/ricbap/sistema/SistemaApplication.java
+++ src/main/java/com/ricbap/sistema/SistemaApplication.java
@@ -1,13 +1,31 @@
 package com.ricbap.sistema;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ricbap.sistema.domain.Categoria;
+import com.ricbap.sistema.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SistemaApplication {
+public class SistemaApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
