860027cf7398fb48c5144f12d24fe76eec70ce54
Ricardo <ric_bap@hotmail.com>
2020-06-26 19:09:09 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/ricbap/sistema/services/CategoriaService.java
+++ src/main/java/com/ricbap/sistema/services/CategoriaService.java
@@ -22,5 +22,10 @@ public class CategoriaService {
 					", Tipo: " + Categoria.class.getName());			
 		});
 	}
+	
+	public Categoria inserir(Categoria categoria) {
+		categoria.setId(null);
+		return categoriaRepository.save(categoria);
+	}
 
 }
