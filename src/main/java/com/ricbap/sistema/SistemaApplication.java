fe5da85a4530fe14711ba3222f18f09361abb107
Ricardo <ric_bap@hotmail.com>
2020-06-26 16:56:59 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/ricbap/sistema/SistemaApplication.java
+++ src/main/java/com/ricbap/sistema/SistemaApplication.java
@@ -13,6 +13,7 @@ import com.ricbap.sistema.domain.Cidade;
 import com.ricbap.sistema.domain.Cliente;
 import com.ricbap.sistema.domain.Endereco;
 import com.ricbap.sistema.domain.Estado;
+import com.ricbap.sistema.domain.ItemPedido;
 import com.ricbap.sistema.domain.Pagamento;
 import com.ricbap.sistema.domain.PagamentoComBoleto;
 import com.ricbap.sistema.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.ricbap.sistema.repositories.CidadeRepository;
 import com.ricbap.sistema.repositories.ClienteRepository;
 import com.ricbap.sistema.repositories.EnderecoRepository;
 import com.ricbap.sistema.repositories.EstadoRepository;
+import com.ricbap.sistema.repositories.ItemPedidoRepository;
 import com.ricbap.sistema.repositories.PagamentoRepository;
 import com.ricbap.sistema.repositories.PedidoRepository;
 import com.ricbap.sistema.repositories.ProdutoRepository;
@@ -55,6 +57,9 @@ public class SistemaApplication implements CommandLineRunner {
 	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 	public static void main(String[] args) {
@@ -122,6 +127,20 @@ public class SistemaApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);	
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
+		
 	}
 
 }
---
+++ src/main/java/com/ricbap/sistema/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.ricbap.sistema.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricbap.sistema.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
