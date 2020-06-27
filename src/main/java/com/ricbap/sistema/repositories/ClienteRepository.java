d348b7f90fa06767f77fc3af21ce56e07f016164
Ricardo <ric_bap@hotmail.com>
2020-06-27 16:05:50 -0300

    Validação customizada: email não repetido na iserçãode cliente

--- src/main/java/com/ricbap/sistema/repositories/ClienteRepository.java
+++ src/main/java/com/ricbap/sistema/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.ricbap.sistema.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.ricbap.sistema.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/ricbap/sistema/services/validation/ClientInsertValidator.java
+++ src/main/java/com/ricbap/sistema/services/validation/ClientInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ricbap.sistema.domain.Cliente;
 import com.ricbap.sistema.domain.enums.TipoCliente;
 import com.ricbap.sistema.dto.ClienteNewDTO;
+import com.ricbap.sistema.repositories.ClienteRepository;
 import com.ricbap.sistema.resources.exception.FieldMessage;
 import com.ricbap.sistema.services.validation.utils.BR;
 
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		}
 		if (clienteNewDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(clienteNewDto.getCpfOuCnpj() )) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(clienteNewDto.getEmail());
+		if ( aux != null ) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		
